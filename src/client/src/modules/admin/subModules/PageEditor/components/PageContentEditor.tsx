cb7a373bc5e359cb800364bf8be43407e74d5713
Alastair <alastairzotos@gmailcom>
2020-07-26 14:19:30 +0100

    site settings split panel

--- src/client/src/modules/admin/subModules/PageEditor/components/PageContentEditor.tsx
+++ src/client/src/modules/admin/subModules/PageEditor/components/PageContentEditor.tsx
@@ -1,22 +1,16 @@
-import { Container, Grid, makeStyles, TextField } from '@material-ui/core';
+import { Container, TextField } from '@material-ui/core';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Panel } from '~/atomic/atoms/Panel';
 import { Spacer } from '~/atomic/atoms/Spacer';
+import SplitPanel from '~/atomic/atoms/SplitPanel';
 
 import { setPageData } from '../actions';
 import { getSelectedPage } from '../selectors';
 
 import { PageSettings } from './settings/PageSettings';
 
-const useStyles = makeStyles(() => ({
-    root: {
-        height: '100%'
-    }
-}));
-
 export const PageContentEditor: React.FC = () => {
-    const classes = useStyles();
-
     const dispatch = useDispatch();
     const selectedPage = useSelector(getSelectedPage);
 
@@ -39,8 +33,8 @@ export const PageContentEditor: React.FC = () => {
     }
 
     return (
-        <Grid container className={classes.root}>
-            <Grid item sm={9}>
+        <SplitPanel id="page" split="vertical" primary="second" minSplit={250}>
+            <Panel>
                 <Container maxWidth="md">
                     <Spacer top={2} />
 
@@ -64,11 +58,10 @@ export const PageContentEditor: React.FC = () => {
                         onChange={e => setContent(e.target.value)}
                     />
                 </Container>
-            </Grid>
-
-            <Grid item sm={3}>
+            </Panel>
+            <Panel>
                 <PageSettings />
-            </Grid>
-        </Grid>
+            </Panel>
+        </SplitPanel>
     );
 };
